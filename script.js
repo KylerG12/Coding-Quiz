6c378c33d4231746c44433692531f3b91183a042
Kyler Gibbs <kylergibbs@hotmail.com>
2024-03-03 13:34:32 -0800

    Added additional JS logic, CSS, and HTML.

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
+//var definitions
+
 var timer = document.getElementById("timer");
-var currentTime = 60; 
+var currentTime = 60;
+var startBtn = document.getElementById("start");
+var titleRem = document.getElementById("Title");
+var questions = [
+  {
+    question: "Which of the following is NOT a primitive value?",
+    options: ["Null", "Boolean", "String", "Sound"],
+    correct: "Sound",
+  },
+  {
+    question: "What element of HTML is not seen by the user?",
+    options: ["<body>", "<img>", "<head>", "<div>"],
+    correct: "<head>",
+  },
+  {
+    question: "A function can be ended with the statement:",
+    options: ["return", "end", "stop", "conclude"],
+    correct: "return",
+  },
+  {
+    question: "Which of the following is a common CSS term?",
+    options: ["control", "padding", "commit", "function"],
+    correct: "padding",
+  },
+];
 
-console.log(timer);
 
-function initCheck(){
-    let score = document.getElementById("ScoresHead");
-    let li = document.createElement("li")
-    li.textContent = "No Highscores yet"
-    if(score.children.length < 1){
-        score.appendChild(li);
+//Function Logic
+
+function initCheck() {
+  let score = document.getElementById("ScoresHead");
+  let li = document.createElement("li");
+  li.textContent = "No Highscores yet";
+  if (score.children.length < 1) {
+    score.appendChild(li);
+  }
+}
+
+function Countdown() {
+    if (currentTime > 55) {
+      currentTime--;
+      timer.textContent = currentTime;
     }
 }
 
-initCheck ();
+function quizStart() {
+    titleRem.setAttribute("class", "hidden");
 
-function Countdown(){
-    currentTime--;
-    timer.textContent = currentTime;
 }
 
-Countdown ();
+//Logic
+initCheck();
+Countdown();
+console.log(startBtn);
+console.log(titleRem);
 
-timerDown = setInterval(Countdown, 1000)
+startBtn.onclick = quizStart;
+
+timerDown = setInterval(Countdown, 1000);
